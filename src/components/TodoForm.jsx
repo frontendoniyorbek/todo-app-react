c6261ca4ab34e526d1809391eb1e87470f84504f
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2023-03-09 15:54:28 +0500

    add vite init and papka components

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const TodoForm = () => {
+    const [input,setInput] = useState('')
+
+  return (
+    <div>TodoForm</div>
+  )
+}
+
+export default TodoForm
